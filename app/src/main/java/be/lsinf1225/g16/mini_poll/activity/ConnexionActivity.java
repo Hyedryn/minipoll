a466f320c5bf697a47c153e713a22f7d30228046
LAPTOP-QDESSAIN\quent <quentinbis@gmx.com>
2018-04-25 14:57:11 +0200

    Fix de quelques layout + debut de l'implémentation de la connexion

--- app/src/main/java/be/lsinf1225/g16/mini_poll/activity/ConnexionActivity.java
+++ app/src/main/java/be/lsinf1225/g16/mini_poll/activity/ConnexionActivity.java
@@ -5,16 +5,19 @@ import android.os.Bundle;
 
 import android.view.View;
 import android.content.Intent;
+import android.app.Activity;
 
 import be.lsinf1225.g16.mini_poll.R;
 
 public class ConnexionActivity extends AppCompatActivity {
 
+    public static Activity login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
-
+        login = this;
     }
 
     public void register(View view)
@@ -27,5 +30,9 @@ public class ConnexionActivity extends AppCompatActivity {
     {
         Intent intent = new Intent(ConnexionActivity.this, MenuMainActivity.class);
         startActivity(intent);
+        finish();
     }
+
+
+
 }

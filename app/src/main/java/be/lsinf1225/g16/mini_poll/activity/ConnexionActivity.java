eae31165a83823822873d367d1ebd7286e71d957
LAPTOP-QDESSAIN\quent <quentinbis@gmx.com>
2018-04-25 16:17:46 +0200

    Creation du main de l'app

--- app/src/main/java/be/lsinf1225/g16/mini_poll/activity/ConnexionActivity.java
+++ app/src/main/java/be/lsinf1225/g16/mini_poll/activity/ConnexionActivity.java
@@ -3,9 +3,12 @@ package be.lsinf1225.g16.mini_poll.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.view.KeyEvent;
 import android.view.View;
 import android.content.Intent;
 import android.app.Activity;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
 
 import be.lsinf1225.g16.mini_poll.R;
 
@@ -33,6 +36,23 @@ public class ConnexionActivity extends AppCompatActivity {
         finish();
     }
 
+
+    /**
+     * Récupère les actions faites depuis le clavier.
+     * <p>
+     * Récupère les actions faites depuis le clavier lors de l'édition du champ du mot de passe afin
+     * de permettre de se connecter depuis le bouton "Terminer" du clavier. (Cela évite à
+     * l'utilisateur de devoir fermer le clavier et de cliquer sur le bouton se connecter).
+     */
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        // L'attribut android:imeOptions="actionNext" est défini dans le fichier xml de layout
+        // (activity_login.xml), L'actionId attendue est donc IME_ACTION_NEXT.
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+            login(v);
+            return true;
+        }
+        return false;
+    }
 
 
 }

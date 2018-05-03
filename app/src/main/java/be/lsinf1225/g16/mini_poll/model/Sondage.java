b9ba23b44770ff837606b5d0c44e0d2fc032c525
Soukeina <soukeina_1342@hotmail.com>
2018-05-03 16:23:53 +0200

    Classe Sondage ajoutée

--- app/src/main/java/be/lsinf1225/g16/mini_poll/model/Sondage.java
+++ app/src/main/java/be/lsinf1225/g16/mini_poll/model/Sondage.java
@@ -1,5 +1,8 @@
 package be.lsinf1225.g16.mini_poll.model;
 
+
+import java.util.ArrayList;
+
 public class Sondage {
 
     protected final Utilisateur createur;
@@ -10,7 +13,7 @@ public class Sondage {
 
     protected Question[] questions;
 
-    protected Utilisateur[] participants;
+    protected ArrayList<Utilisateur> participants;
 
     protected enum Etat {
         ACTIF,
@@ -42,4 +45,51 @@ public class Sondage {
         return this.sondageId;
     }
 
+    public void setCreateur(Utilisateur x) {this.createur = x;}
+
+    public Utilisateur getCreateur() {return this.createur;}
+
+    public void setParticipant(Utilisateur name) {this.participant = name;}
+
+    public Utilisateur getParicipant() {return this.participant;}
+
+    public void setListeParticipants(Utilisateur[] liste)
+    {
+        for (int i = 0; i < liste.length(); i++){
+            participants.add(liste[i]);
+        }
+    }
+
+    public ArrayList<Utilisateur> getListeParticipants() {return this.participants;}
+
+    public boolean getState() {return this.etat;}
+
+    public void setQuestions(Question[] q) {this.questions = q;}
+
+    public Question[] getQuestions() {return this.questions;}
+
+    public void setChoix(Choix[] c) {this.choix = c;}
+
+    public Choix[] getChoix() {return this.choix;}
+
+    public String getType() {return this.t;}
+
+    public boolean isCreatedBy(String name)
+    {
+        if (name.equals(createur.getIdentifiant())){
+            return true;
+        }
+        return false;
+    }
+
+    public void addParticipants(Utilisateur ami) {this.participants.add(ami);}
+
+    public void addQuestion(Question question)
+    {
+        for (int i = 0; i < this.questions.length(); i++){
+            if (this.questions[i] == null){
+                this.questions[i] = question;
+            }
+        }
+    }
 }
